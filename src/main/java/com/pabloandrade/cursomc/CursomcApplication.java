06aea5500ee71133542f3a2947043c5e4e2b85a8
Pablo Andrade <pabllo007@gmail.com>
2019-09-06 17:19:11 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/pabloandrade/cursomc/CursomcApplication.java
+++ src/main/java/com/pabloandrade/cursomc/CursomcApplication.java
@@ -113,12 +113,12 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
 		
-		Pedido ped1 = new Pedido(null, sdf.parse("09/05/2019 20:59"), cli1, e1);
-		Pedido ped2 = new Pedido(null, sdf.parse("10/05/2019 21:01"), cli1, e2);
+		Pedido ped1 = new Pedido(null, sdf.parse("06/09/2019 17:12"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("06/09/2019 17:12"), cli1, e2);
 		
 		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pagto1);
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2019 12:00"), null);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("06/09/2019 17:12"), null);
 		ped2.setPagamento(pagto2);
 		
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
