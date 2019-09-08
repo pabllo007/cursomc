c2fe56b8e10aaafcf8c5a57309e35ca05f4ad5e7
Pablo Andrade <pabllo007@gmail.com>
2019-09-08 15:11:02 -0300

    Validação sintátic com Bean Validation

--- src/main/java/com/pabloandrade/cursomc/services/CategoriaService.java
+++ src/main/java/com/pabloandrade/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.pabloandrade.cursomc.domain.Categoria;
+import com.pabloandrade.cursomc.dto.CategoriaDTO;
 import com.pabloandrade.cursomc.repositories.CategoriaRepository;
 import com.pabloandrade.cursomc.services.exceptions.DataIntegrityException;
 import com.pabloandrade.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
 		return repo.findAll(pageRequest);
+	}
+	
+	public 	Categoria fromDTO(CategoriaDTO dto) {
+		return new Categoria(dto.getId(), dto.getNome());
 	}
 }
