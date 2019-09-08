919df40be86177ad78d11a6597dab2834c113f03
Pablo Andrade <pabllo007@gmail.com>
2019-09-08 15:54:47 -0300

    Personalizando o retorno de erro da validação

---
+++ src/main/java/com/pabloandrade/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,50 @@
+package com.pabloandrade.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.pabloandrade.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	
+	@NotEmpty(message = "O campo nome é obrigatório")
+	@Size(min = 5, max = 80, message = "O campo nome deve ter entre 5 e 80 caracteres")
+	private String nome;
+
+	public CategoriaDTO() {
+	}
+
+	public CategoriaDTO(Categoria obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+	}
+
+	public CategoriaDTO(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
