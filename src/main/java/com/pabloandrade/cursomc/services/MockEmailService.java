c9ff843cf75874665bb5563d55fef09b50ed91a9
= <=>
2019-09-10 18:26:02 -0300

    Email HTML

--- src/main/java/com/pabloandrade/cursomc/services/MockEmailService.java
+++ src/main/java/com/pabloandrade/cursomc/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.pabloandrade.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -7,13 +9,21 @@ import org.springframework.mail.SimpleMailMessage;
 public class MockEmailService extends AbstractEmailService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
-	
+
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 
-		LOG.info("SIMULANDO ENVIO DE E-MAIL");
+		LOG.info("SIMULANDO ENVIO DE E-MAIL...");
 		LOG.info(msg.toString());
 		LOG.info("EMAIL ENVIADO");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("SIMULANDO ENVIO DE E-MAIL HTML...");
+		LOG.info(msg.toString());
+		LOG.info("EMAIL ENVIADO");
+
 	}
 
 }
