200bdbc681fa9f367cc7dadf9b5e194764a8628b
= <=>
2019-09-10 15:53:06 -0300

    Implementando toString, NumberFormat, SimpleDateFormat

--- src/main/java/com/pabloandrade/cursomc/services/PedidoService.java
+++ src/main/java/com/pabloandrade/cursomc/services/PedidoService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pabloandrade.cursomc.domain.ItemPedido;
 import com.pabloandrade.cursomc.domain.PagamentoComBoleto;
@@ -33,6 +34,9 @@ public class PedidoService {
 	@Autowired
 	ItemPedidoRepository itemPedidoRepository;
 
+	@Autowired
+	private ClienteService clienteService;
+	
 	public Pedido find(Integer id) {
 
 		Optional<Pedido> obj = pedidoRepository.findById(id);
@@ -42,9 +46,11 @@ public class PedidoService {
 
 	}
 
+	@Transactional
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -56,11 +62,13 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 
 	}
