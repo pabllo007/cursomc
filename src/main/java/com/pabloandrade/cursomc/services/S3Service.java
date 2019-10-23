d9a2566f85b60f8443432db4c831d741d11ae979
= <=>
2019-10-23 14:20:50 -0300

    tratamento de excessoes de file e amazon3

--- src/main/java/com/pabloandrade/cursomc/services/S3Service.java
+++ src/main/java/com/pabloandrade/cursomc/services/S3Service.java
@@ -5,8 +5,6 @@ import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import javax.management.RuntimeErrorException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.pabloandrade.cursomc.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -37,7 +36,7 @@ public class S3Service {
 			return uploadFile(is, fileName, contentType);
 
 		} catch (IOException e) {
-			throw new RuntimeException("Erro IO: " + e.getMessage());
+			throw new FileException("Erro IO: " + e.getMessage());
 		}
 	}
 
@@ -54,7 +53,7 @@ public class S3Service {
 			LOG.info("Finalizando o upload na Amazon");
 			return s3Client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 
 	}
