527ee6a9b9a1e43c6871152bda369b853a053d2b
= <=>
2019-10-23 14:10:20 -0300

    enviando imagem para Amazon via endpoit

--- src/main/java/com/pabloandrade/cursomc/CursomcApplication.java
+++ src/main/java/com/pabloandrade/cursomc/CursomcApplication.java
@@ -1,17 +1,14 @@
 package com.pabloandrade.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.pabloandrade.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private S3Service s3Service;
+//	@Autowired
+//	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -20,9 +17,9 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		String localFilePath = "C:\\Users\\pablo.andrade\\Pictures\\Camera Roll\\pablo.jpg";
-		
-		s3Service.uploadFile(localFilePath);
+//		String localFilePath = "C:\\Users\\pablo.andrade\\Pictures\\Camera Roll\\pablo.jpg";
+//		
+//		s3Service.uploadFile(localFilePath);
 	}
 
 }
--- src/main/java/com/pabloandrade/cursomc/services/S3Service.java
+++ src/main/java/com/pabloandrade/cursomc/services/S3Service.java
@@ -1,17 +1,21 @@
 package com.pabloandrade.cursomc.services;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.management.RuntimeErrorException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 
 @Service
 public class S3Service {
@@ -24,21 +28,34 @@ public class S3Service {
 	@Value("${s3.bucket}")
 	private String bucketName;
 
-	public void uploadFile(String localFilePath) {
+	public URI uploadFile(MultipartFile multipartFile) {
 		try {
-			
-			LOG.info(bucketName);
+			String fileName = multipartFile.getOriginalFilename();
+			InputStream is = multipartFile.getInputStream();
+			String contentType = multipartFile.getContentType();
+
+			return uploadFile(is, fileName, contentType);
+
+		} catch (IOException e) {
+			throw new RuntimeException("Erro IO: " + e.getMessage());
+		}
+	}
+
+	public URI uploadFile(InputStream is, String fileName, String contentType) {
+
+		try {
 			LOG.info("Pegando o arquivo");
-			File file = new File(localFilePath);
+			ObjectMetadata meta = new ObjectMetadata();
+			meta.setContentType(contentType);
+			s3Client.putObject(bucketName, fileName, is, meta);
+
 			LOG.info("Arquivo recuperado");
 			LOG.info("Inciando o upload na Amazon");
-			s3Client.putObject(new PutObjectRequest(bucketName, file.getName(), file));
 			LOG.info("Finalizando o upload na Amazon");
-		} catch (AmazonServiceException e) {
-			LOG.info("AmazonServiceException: " + e.getMessage());
-			LOG.info("Status code: " + e.getErrorCode());
-		} catch (AmazonClientException e) {
-			LOG.info("AmazonClientException: " + e.getMessage());
+			return s3Client.getUrl(bucketName, fileName).toURI();
+		} catch (URISyntaxException e) {
+			throw new RuntimeException("Erro ao converter URL para URI");
 		}
+
 	}
 }
