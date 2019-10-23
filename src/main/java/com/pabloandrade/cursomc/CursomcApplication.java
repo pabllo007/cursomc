9bcbbf1b306d61d27e06cdf25dae09e6a5ee1a18
= <=>
2019-10-23 11:24:15 -0300

    salvando arquivo no s3 da amazon

--- src/main/java/com/pabloandrade/cursomc/CursomcApplication.java
+++ src/main/java/com/pabloandrade/cursomc/CursomcApplication.java
@@ -1,12 +1,18 @@
 package com.pabloandrade.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.pabloandrade.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -14,6 +20,9 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		String localFilePath = "C:\\Users\\pablo.andrade\\Pictures\\Camera Roll\\pablo.jpg";
+		
+		s3Service.uploadFile(localFilePath);
 	}
 
 }
