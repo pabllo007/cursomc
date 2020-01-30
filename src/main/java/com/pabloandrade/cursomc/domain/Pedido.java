dd0d08ff3785e61d45df9788bdc0550ed4965a2d
Pablo <pablo.andrade@tty2000.com.br>
2020-01-30 12:18:24 -0300

    correção na geração de identity das tabelas

--- src/main/java/com/pabloandrade/cursomc/domain/Pedido.java
+++ src/main/java/com/pabloandrade/cursomc/domain/Pedido.java
@@ -26,7 +26,7 @@ public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
--- src/main/java/com/pabloandrade/cursomc/domain/Produto.java
+++ src/main/java/com/pabloandrade/cursomc/domain/Produto.java
@@ -22,7 +22,7 @@ public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private Double preco;
